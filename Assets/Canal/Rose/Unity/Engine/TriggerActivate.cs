766014460ad0718a05ceb52536e7a30357bdfdfd
Don Arboleda <darboleda@zynga.com>
2014-08-29 16:56:09 -0700

    Trigger activation and animations.

---
+++ Assets/Canal/Rose/Unity/Engine/TriggerActivate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Canal.Unity;
+
+namespace Canal.Rose.Unity.Engine
+{
+    public class TriggerActivate : Behavior
+    {
+        public GameObject target;
+        public bool invert = false;
+        public bool setOnEnter = true;
+        public bool setOnExit = true;
+
+        public void Awake()
+        {
+            target.SetActive(invert);
+        }
+        
+        public void OnCanalTriggerEnter(Trigger trigger)
+        {
+            if(setOnEnter)
+            {
+                target.SetActive(!invert);
+            }
+        }
+
+        public void OnCanalTriggerExit(Trigger trigger)
+        {
+            if(setOnExit)
+            {
+                target.SetActive(invert);
+            }
+        }
+    }
+}
---
+++ Assets/Canal/Rose/Unity/Engine/TriggerPlayAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Canal.Unity;
+
+namespace Canal.Rose.Unity.Engine
+{
+    public class TriggerPlayAnimation : Behavior
+    {
+        public Animator Animator;
+        public string AnimatorEnterTrigger;
+        public string AnimatorExitTrigger;
+        
+        public void OnCanalTriggerEnter(Trigger trigger)
+        {
+            if (!string.IsNullOrEmpty(AnimatorEnterTrigger)) Animator.SetTrigger(AnimatorEnterTrigger);
+        }
+        
+        public void OnCanalTriggerExit(Trigger trigger)
+        {
+            if (!string.IsNullOrEmpty(AnimatorEnterTrigger)) Animator.SetTrigger(AnimatorExitTrigger);
+        }
+    }
+}
