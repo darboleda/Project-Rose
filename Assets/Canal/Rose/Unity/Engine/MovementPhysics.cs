9369cdb5ff719dd0f6b318e2ae9bd0a41eaa14f8
Mors <morsangelos@hotmail.com>
2014-08-31 13:55:19 -0700

    Separated PlayerController into PlayerInput and MovementPhysics

--- Assets/Canal/Rose/Unity/Engine/MovementPhysics.cs
+++ Assets/Canal/Rose/Unity/Engine/MovementPhysics.cs
@@ -7,10 +7,13 @@ using Canal.Unity;
 
 namespace Canal.Rose.Unity.Engine
 {
-    public class PlayerController : Behavior {
+    public class MovementPhysics : Behavior {
         public Tracer Tracer;
 
-        public float WalkAcceleration = 1f;
+        [System.NonSerialized]
+        public float WalkDirection = 0f;
+
+        public float WalkAcceleration = 2f;
         public float WalkSpeed = 2f;
         public float MomentumRatio = 0.5f;
 
@@ -21,34 +24,47 @@ namespace Canal.Rose.Unity.Engine
 
         public bool activateTriggers = true;
 
+        private float currentAcceleration = 0;
         private float currentSpeed = 0;
 
         private HashSet<Trigger> currentTriggers = new HashSet<Trigger>();
 
         public void FixedUpdate()
         {
-            float move = Input.GetAxisRaw("Horizontal");
-            if (move != 0)
+            ApplyAcceleration();
+            ApplyVelocity();
+            CheckTriggers();
+        }
+
+        public void ApplyAcceleration()
+        {
+            if (WalkDirection != 0)
             {
-                currentSpeed = Mathf.Clamp(currentSpeed + (move * WalkAcceleration * Time.deltaTime), -WalkSpeed, WalkSpeed);
+                currentSpeed = Mathf.Clamp(currentSpeed + (WalkDirection * WalkAcceleration * Time.deltaTime), -WalkSpeed, WalkSpeed);
             }
             else
             {
                 currentSpeed *= MomentumRatio;
             }
+        }
 
+        public void ApplyVelocity()
+        {
             transform.position = Tracer.Move(currentSpeed * Time.deltaTime);
-            Animator.SetFloat("hSpeed", move * WalkSpeed);
+            Animator.SetFloat("hSpeed", currentSpeed);
             Animator.SetBool("Walking", Mathf.Abs(currentSpeed) > 0.5f);
 
-            if (move != 0)
+            if (currentSpeed != 0)
             {
-                Vector3 facing = Tracer.GetFacingDirection(move);
+                Vector3 facing = Tracer.GetFacingDirection(Mathf.Sign(currentSpeed));
                 facing.y = 0;
                 facing.Normalize();
                 FacingTarget.rotation = Quaternion.FromToRotation(Vector3.right, facing);
             }
+        }
 
+        public void CheckTriggers()
+        {
             if (activateTriggers)
             {
                 List<Trigger> newTriggers = new List<Trigger>(Tracer.GetTriggers(this.Radius));
---
+++ Assets/Canal/Rose/Unity/Engine/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+
+using Canal.Rose.Unity.Engine;
+using Canal.Unity;
+
+namespace Canal.Rose.Unity.Engine
+{
+    public class PlayerInput : Behavior {
+        public MovementPhysics Physics;
+
+        public void Update()
+        {
+            float move = Input.GetAxisRaw("Horizontal");
+            Physics.WalkDirection = (move == 0 ? 0 : Mathf.Sign(move));
+        }
+    }
+}
